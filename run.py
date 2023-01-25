285094ad8a20c341bd5729d3dc76702ad858fdfc
Avishav-Engle <82023333+Avishav-Engle@users.noreply.github.com>
2023-01-26 00:16:40 +0200

    Update and rename detect.py to run.py

--- run.py
+++ run.py
@@ -13,13 +13,13 @@ import torchvision
 from sklearn.cluster import KMeans
 import numpy as np
 
-from models.experimental import attempt_load
-from utils.datasets import LoadStreams, LoadImages
-from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+from yolov7-main.models.experimental import attempt_load
+from yolov7-main.utils.datasets import LoadStreams, LoadImages
+from yolov7-main.utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
     scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
-from utils.plots import plot_one_box
-from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
-from our_functions.Our_Functions import my_transforms, BYOL
+from yolov7-main.utils.plots import plot_one_box
+from yolov7-main.utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
+from BYOL-main import my_transforms, BYOL
 
 
 def detect(save_img=False):
